6e75895d5cd4ad9d784ff0a7cfd7f08cada53218
kiranmayi9 <82269929+kiranmayi9@users.noreply.github.com>
2021-04-29 16:55:17 +0530

    positive num.py
    This program prints the positive numbers in a range on the screen

---
+++ positive num.py
@@ -0,0 +1,10 @@
+list1 = int(input("enter the list1 of range: "))
+output = int(input("enter the output of the range: "))
+for num in range(list1, output + 1):
+    if num >= 0:
+       print(num, end = " ")
+list2 = int(input("enter the list2 of range: "))
+output = int(input("enter the output of the range: "))
+for num in range(list2, output + 1):
+   if num >= 0:
+      print(num, end = " ")
