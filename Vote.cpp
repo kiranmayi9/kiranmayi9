e758991b89b3eb6d10e9c018b61b99c5724e46af
kiranmayi9 <82269929+kiranmayi9@users.noreply.github.com>
2021-05-17 20:50:05 +0530

    created vote.cpp

---
+++ Vote.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int age;
+    cout <<"enter the age :";
+    cin >>age;
+ 
+    if (age>=18)
+    {
+ 	  cout<<"eligible to vote";
+    } 
+    else
+    {
+ 	  cout<<"not eligible to vote";
+    }	 
+  return 0;
+}
+ 
+
